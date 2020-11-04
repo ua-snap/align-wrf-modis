eea114e3dc97fc59797403d6044c42884439b446
Kyle <kmredilla@alaska.edu>
2020-11-03 18:10:40 -0900

    apply new paradigm for determining future modis dates

--- alignment_pipeline/make_future_modis_periods.py
+++ alignment_pipeline/make_future_modis_periods.py
@@ -9,58 +9,60 @@ import numpy as np
 from helpers import check_env
 
 
-def make_write_future_ranges(df, out_fp, periods):
+def make_write_future_ranges(temp_fp, period):
     """
-    Make and save data frame of future ranges for specified year
+    Make and save data frame of future ranges for specified years. 
+    Note: this does not need to be based on historical period, because a 
+    new 8-day period is started on the first of every year. 
     """
-    prev_date = df["RANGEBEGINNINGDATE"].values[-1]
-    begin_time = df["RANGEBEGINNINGTIME"].values[-1]
-    end_time = df["RANGEENDINGTIME"].values[-1]
-    begin_range = pd.date_range(prev_date, "2078-01-08", freq="8D")
-    begin_dates = pd.to_datetime(begin_range.values)
-    end_dates = (begin_dates - pd.Timedelta(seconds=1))[1:]
-    begin_dates = begin_dates[:-1]
+    def make_dates(year):
+        """
+        Make starting dates for a given year.
+        Use end cutoff of 11/1. Using 10/31 as end date results in extra period
+        for leap years. This final period will always be only days from November
+        excep on leap years.
+        """
+        begin_dates = pd.date_range(f"{year}-01-01", f"{year}-11-01", freq="8D")
+        return begin_dates.values
+
+    years = np.arange(int(period[0]), int(period[1]) + 1)
+    begin_dates = np.array([make_dates(year) for year in years]).flatten()
+    begin_dates = pd.to_datetime(begin_dates)
+    end_dates = begin_dates + pd.Timedelta(days=7)
     times_df = pd.DataFrame(
         {
             "RANGEBEGINNINGDATE": begin_dates.strftime("%Y-%m-%d"),
-            "RANGEBEGINNINGTIME": begin_time,
+            "RANGEBEGINNINGTIME": "00:00:00",
             "RANGEENDINGDATE": end_dates.strftime("%Y-%m-%d"),
-            "RANGEENDINGTIME": end_time,
+            "RANGEENDINGTIME": "23:59:59",
         },
-        index=begin_range[:-1],
+        # index=begin_range[:-1],
+        index=begin_dates
     )
 
-    # find start and end days-of-year for subsetting to relevant
-    #  time of year for models (growing season: April - October)
+    # find start day-of-year for subsetting to relevant
+    # time of year for models (growing season starts in April)
     start_doy = datetime.date(2020, 3, 25).timetuple().tm_yday
-    # finding yday of last day of October should be done with
-    #  leap year to ensure inclusion of all possible periods starting
-    #  in October
-    end_doy = datetime.date(2020, 10, 31).timetuple().tm_yday
-    times_df = times_df.loc[
-        (times_df.index.dayofyear >= start_doy) & (times_df.index.dayofyear <= end_doy)
-    ]
+    times_df = times_df.loc[(times_df.index.dayofyear >= start_doy)]
+    out_fp = temp_fp.format(period[0], period[1])
+    times_df.to_csv(out_fp, index=False)
 
-    # slice and save for each future period
-    for years in periods:
-        print("saving file as ", out_fp.format(years[0], years[1]))
-        times_df[years[0] : years[1]].to_csv(out_fp.format(years[0], years[1]), index=False)
+    return out_fp
 
 
 if __name__ == "__main__":
-    env_ok = check_env(wrf_env_var=False)
-    if not env_ok:
-        exit("Environment variables incorrectly setup, check README for requirements")
+    env_ok = check_env()
     # read historical meta data to get last date of MODIS to continue series into future
     scratch_dir = os.getenv("SCRATCH_DIR")
     ancillary_dir = os.path.join(scratch_dir, "ancillary")
-    meta_fp = os.path.join(
-        ancillary_dir, "historical_modis_range_metadata.csv"
-    )
-    df = pd.read_csv(meta_fp)
+    # meta_fp = os.path.join(
+    #     ancillary_dir, "historical_modis_range_metadata.csv"
+    # )
+    # df = pd.read_csv(meta_fp)
 
     # Future WRF periods are 2037-07-02 - 2047-12-31, and 2067-07-02 - 2077-12-31
     future_periods = [("2037", "2047"), ("2067", "2077")]
-    out_fp = os.path.join(ancillary_dir, "future_modis_range_metadata_{}-{}.csv")
-
-    make_write_future_ranges(df, out_fp, future_periods)
+    temp_fp = os.path.join(ancillary_dir, "future_modis_range_metadata_{}-{}.csv")
+    for period in future_periods:
+        out_fp = make_write_future_ranges(temp_fp, period)
+        print(f"{period} date ranges saved as {out_fp}")
