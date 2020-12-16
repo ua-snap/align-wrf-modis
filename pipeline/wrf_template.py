112340cb117956f81972d87cbc3960da82d573f5
Kyle <kmredilla@alaska.edu>
2020-12-16 11:07:15 -0900

    crop to extent of data

--- pipeline/wrf_template.py
+++ pipeline/wrf_template.py
@@ -6,15 +6,38 @@ import rasterio as rio
 from helpers import check_env
 
 
+def crop_nodata(fp):
+    """Crop GeoTIFF to extent of valid data"""
+    with rio.open(fp) as src:
+        arr = src.read(1)
+        meta = src.meta
+        psize = src.xy(0,1)[0] - src.xy(0,0)[0]
+    valid = arr != 0
+    valid_idx = np.where(valid)
+    rmin, rmax = min(valid_idx[0]), max(valid_idx[0]) + 1
+    cmin, cmax = min(valid_idx[1]), max(valid_idx[1]) + 1
+    # new data cropped to extent of valid data
+    arr = arr[rmin:rmax,cmin:cmax]
+    # create new metadata for cropped data and overwrite
+    west, north = rio.transform.xy(meta["transform"], rmin, cmin)
+    meta["transform"] = rio.transform.from_origin(west, north, psize, psize)
+    meta["height"], meta["width"] = arr.shape
+    os.unlink(fp)
+    with rio.open(fp, "w", **meta) as src:
+        src.write(arr, 1)
+    return None
+
+
 def reproject_band(arr, meta, band_fp):
     """run the reprojection for a template WRF band"""
-    proj_fp = band_fp.replace("4326", "3338")
     with rio.open(band_fp, "w", **meta) as src:
         src.write(arr, 1)
+    proj_fp = band_fp.replace("4326", "3338")
     _ = subprocess.call(
         ["gdalwarp", "-t_srs", "epsg:3338", "-q", "-overwrite", band_fp, proj_fp]
     )
     os.unlink(band_fp)
+    crop_nodata(proj_fp)
     return proj_fp
 
 
@@ -31,7 +54,7 @@ if __name__ == "__main__":
         band_arr = src.read(1)
         meta = src.meta
     meta.update(
-        {"driver": "GTiff", "nodata": -9999,}
+        {"driver": "GTiff", "nodata": 0,}
     )
     band_fp = os.path.join(scratch_dir, "ancillary", "wrf_4326_template.tif")
     proj_fp = reproject_band(band_arr, meta, band_fp)
